d200fcd267e946ec4cb273a660a70cbfbbac0fcf
ujjwalkumar-64 <ujjwalkumar6281@gmail.com>
2026-02-06 02:58:53 +0530

    initial commit

---
+++ src/main/java/com/apora/eventweatherguard/EventWeatherGuardApplication.java
@@ -0,0 +1,13 @@
+package com.apora.eventweatherguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventWeatherGuardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventWeatherGuardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apora/eventweatherguard/EventWeatherGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apora.eventweatherguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventWeatherGuardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
